f2f0a82ab3eba27a5179fb1f3a14f1bdd95dc74c
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-02-08 19:26:01 +0200

    Setup postgres db and prisma.

---
+++ src/prisma/db.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+export * from "@prisma/client";
+
+let cachedPrisma: PrismaClient | undefined = undefined;
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!cachedPrisma) {
+    cachedPrisma = new PrismaClient();
+  }
+
+  prisma = cachedPrisma;
+}
+
+export const db = prisma;
